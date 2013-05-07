985a1378c7421ef0ee4dbf6e6d944cadc9e538cd
Andrew Hofstetter (ahofstetter) <ahofs@guidance.com>
2013-05-07 11:26:57 -0700

    Initial commit of the Magento Cachebuster Lightspeed module.

---
+++ src/app/code/community/Guidance/Lscachebuster/Helper/Data.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Helper
+ *
+ * @author     Guidance Magento Team <magento@guidance.com>
+ * @category   Guidance
+ * @package    Cachebuster
+ * @copyright  Copyright (c) 2013 Guidance Solutions (http://www.guidance.com)
+ * @license    http://www.apache.org/licenses/LICENSE-2.0  Apache License 2.0
+ */
+class Guidance_Lscachebuster_Helper_Data extends Mage_Core_Helper_Data
+{
+
+}
